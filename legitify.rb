da2da26ff9caa3233a841c4f2525ae7fba50c58b
nadav-legit <107790206+nadav-legit@users.noreply.github.com>
2023-07-27 11:08:23 +0300

    Update legitify.rb

--- legitify.rb
+++ legitify.rb
@@ -1,22 +1,20 @@
 
 class Legitify < Formula
-desc "Legitify - open source scm scanning tool by Legit Security"
-homepage "https://github.com/Legit-Labs/legitify"
-
-on_macos do
+    desc "Legitify - open source scm scanning tool by Legit Security"
+    homepage "https://github.com/Legit-Labs/legitify"
+    
     if Hardware::CPU.intel?
-    url "https://legitify.legitsecurity.com/0.2.8/darwin/amd64.tar.gz"
-    sha256 "5d4ceef28dff4d80149c634fb4fc74e35c25bb3395b9ec4b090f53b445e5ddfa"
-    version "0.2.8"
+        url "https://legitify.legitsecurity.com/0.2.8/darwin/amd64.tar.gz"
+        sha256 "5d4ceef28dff4d80149c634fb4fc74e35c25bb3395b9ec4b090f53b445e5ddfa"
+        version "0.2.8"
     end 
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://legitify.legitsecurity.com/0.2.8/darwin/arm64.tar.gz"
-    sha256 "dccd1b83fb34c39d1a0668856bd38e95f0dc51d8ae4a51a96967888acd6865e1"
-    version "0.2.8" 
+        url "https://legitify.legitsecurity.com/0.2.8/darwin/arm64.tar.gz"
+        sha256 "dccd1b83fb34c39d1a0668856bd38e95f0dc51d8ae4a51a96967888acd6865e1"
+        version "0.2.8" 
     end
-end
-
+    
     def install
-    bin.install "legitify"
+        bin.install "legitify"
     end
 end
