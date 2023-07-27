9290bf504a394abacb6ce493ddb598c672463c9a
nadav-legit <107790206+nadav-legit@users.noreply.github.com>
2023-07-27 12:15:39 +0300

    Update legitify.rb

--- legitify.rb
+++ legitify.rb
@@ -3,7 +3,6 @@ class Legitify < Formula
     desc "Legitify - open source scm scanning tool by Legit Security"
     homepage "https://github.com/Legit-Labs/legitify"
   
-  on_macos do
     if Hardware::CPU.intel?
         url "https://legitify.legitsecurity.com/0.2.8/darwin/amd64.tar.gz"
         sha256 "5d4ceef28dff4d80149c634fb4fc74e35c25bb3395b9ec4b090f53b445e5ddfa"
@@ -14,7 +13,6 @@ class Legitify < Formula
         sha256 "dccd1b83fb34c39d1a0668856bd38e95f0dc51d8ae4a51a96967888acd6865e1"
         version "0.2.8" 
     end
-  end
     
     def install
         bin.install "legitify"
