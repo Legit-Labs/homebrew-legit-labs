b5980e56261d911dd871914a521c719d42276e45
legitify-releaser <releaser@legitsecurity.com>
2023-07-31 09:33:01 +0000

    Bump brew formula

--- legitify.rb
+++ legitify.rb
@@ -2,18 +2,18 @@
 class Legitify < Formula
     desc "Legitify - open source scm scanning tool by Legit Security"
     homepage "https://github.com/Legit-Labs/legitify"
-  
+
     if Hardware::CPU.intel?
-        url "https://legitify.legitsecurity.com/0.2.8/darwin/amd64.tar.gz"
-        sha256 "5d4ceef28dff4d80149c634fb4fc74e35c25bb3395b9ec4b090f53b445e5ddfa"
-        version "0.2.8"
+        url "https://legitify.legitsecurity.com/1.0.0/darwin/amd64.tar.gz"
+        sha256 "879bc12660635af3a891c6d6a0380741da6c0a74fe99f0175a212f7160444c7d"
+        version "1.0.0"
     end 
     if Hardware::CPU.arm?
-        url "https://legitify.legitsecurity.com/0.2.8/darwin/arm64.tar.gz"
-        sha256 "dccd1b83fb34c39d1a0668856bd38e95f0dc51d8ae4a51a96967888acd6865e1"
-        version "0.2.8" 
+        url "https://legitify.legitsecurity.com/1.0.0/darwin/arm64.tar.gz"
+        sha256 "ab3ea36f0eb2394e825fab41fdf57bc02e2c7f4cff9214ddc24cddb8ce603ace"
+        version "1.0.0" 
     end
-    
+
     def install
         bin.install "legitify"
     end
