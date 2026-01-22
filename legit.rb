5b63681a06e0c115e6509308f869d3c15a9f7c95
Talbalash-legit <talb@legitsecurity.com>
2026-01-22 14:50:08 +0200

    Add Legit formula for CLI tool

---
+++ legit.rb
@@ -0,0 +1,37 @@
+# typed: false
+# frozen_string_literal: true
+
+class Legit < Formula
+  desc "CLI tool for scanning code with Legit Security - secrets, SCA, and SAST"
+  homepage "https://www.legitsecurity.com"
+  version "1.0.62"
+  license :cannot_represent
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://legit-cli.s3.amazonaws.com/legit/v#{version}/legit_#{version}_darwin_arm64.tar.gz"
+      sha256 "556f1ce45a2d2a96af87c3afe6ff7b2e301e99758d2cb9c696da525d53a6d224"
+    else
+      url "https://legit-cli.s3.amazonaws.com/legit/v#{version}/legit_#{version}_darwin_amd64.tar.gz"
+      sha256 "56c6990b604183dd0d5789eeacd8d861f9b09117772efcf5a733b7f40d7db7bd"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://legit-cli.s3.amazonaws.com/legit/v#{version}/legit_#{version}_linux_arm64.tar.gz"
+      sha256 "afd2a17b59ff59dcd1e00a442198487d86530994847ff705c1e5fa975416d70e"
+    else
+      url "https://legit-cli.s3.amazonaws.com/legit/v#{version}/legit_#{version}_linux_amd64.tar.gz"
+      sha256 "b1d3ef036a6404c0f68030c36785bc275456dd60b0f7150bb83ecd0a9fb1d4b6"
+    end
+  end
+
+  def install
+    bin.install "legit"
+  end
+
+  test do
+    assert_match "legit version", shell_output("#{bin}/legit version")
+  end
+end
